79aacd8953cb3b25290c44bdf41e65c03a64d3eb
Egle <egle.katilauskaite@gmail.com>
2019-12-05 16:23:51 +0000

    Add some route handling

---
+++ node-request-client/app/routes/person.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req,res,next) => res.send("Respond with a resource"));
+
+module.exports = router;
--- node-request-client/app/server.js
+++ node-request-client/app/server.js
@@ -10,11 +10,18 @@
 */
 
 
+
 const express = require('express');
+const path = require('path');
+const indexRouter = require("/");
+const personRouter = require('/person');
+
 const app = express();
 const port = 8080;
 
 express.static('public'); //specify the route of static files (html/css/js)
+app.use("/", indexRouter);
+app.use('/', personRouter);
 
 app.get('/', (req,res) => res.sendFile("/form.html"));
 
@@ -22,6 +29,4 @@ app.get('/', (req,res) => res.sendFile("/form.html"));
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.use((req,res,next) => res.status(404).send("404: Not Found"));  //specify what to do when 404 err happens
-
-
 
