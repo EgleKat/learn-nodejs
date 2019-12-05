0f3d424736a0ae9cf3269e073e2e119460ca0115
Egle <egle.katilauskaite@gmail.com>
2019-12-05 18:37:19 +0000

    Test

--- node-request-client/app/server.js
+++ node-request-client/app/server.js
@@ -13,8 +13,8 @@
 
 const express = require('express');
 const path = require('path');
-//const indexRouter = require("/");
-//const personRouter = require('/person');
+const indexRouter = require("./routes/index");
+const personRouter = require('./routes/person');
 
 const app = express();
 const port = 8080;
@@ -23,7 +23,7 @@ express.static('public'); //specify the route of static files (html/css/js)
 //app.use("/", indexRouter);
 //app.use('/', personRouter);
 
-app.get('/', (req,res) => res.sendFile("/form.html"));
+app.get('/', (req,res) => res.sendFile("public/form.html"));
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
