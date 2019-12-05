2ca48b65e007ea5a328293c098be5dadc40fa6c7
Egle <egle.katilauskaite@gmail.com>
2019-12-05 15:04:56 +0000

    Add basic express  module http code

--- node-request-client/server.js
+++ node-request-client/server.js
@@ -1,7 +1,20 @@
-var http = require('http');
+/*Basic HTTP APP
 
-http.createServer(function (req, res) { //create a server
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(8080);
+// var http = require('http');
+
+// http.createServer(function (req, res) { //create a server
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.write('Hello World!');
+//   res.end();
+// }).listen(8080);
+*/
+
+
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req,res) => res.send("Hello World"));
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
 
